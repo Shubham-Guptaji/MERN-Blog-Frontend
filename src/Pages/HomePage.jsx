cd1d84770acd8c9a4d47d86d88ba144e950f6b0a
Shubham Gupta <shubhamguptaji81@gmail.com>
2024-03-19 22:21:44 +0530

    Started Blog Fronted

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getHomePagePosts } from "../Redux/blogSlice";
+
+const HomePage = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <>
+        </>
+    )
+}
+export default HomePage;
---
+++ src/Redux/blogSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+import axiosInstance from "../Helper/axiosInstance";
+
+const initialState = {
+    trendingPosts: [],
+    popularAuthorPosts: [],
+    tags: [],
+}
+
+// function to get all the home page blog posts
+export const getHomePagePosts = createAsyncThunk("/get", async () => {
+    try {
+        const res = axiosInstance.get("/blogs/");
+
+        const response = await res;
+        console.log(response.data);
+        return response.data;
+    } catch (err) {
+        toast.error(err?.response?.data?.message);
+    }
+})
+
+
+
+// Slice for Blog Api
+
+const blogSlice = createSlice({
+    name: "blog",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getHomePagePosts.fulfilled, (state, action) => {
+            if(action.payload){
+                state.trendingPosts = [...action.payload.trendingPosts];
+                state.popularAuthorPosts  = [...action.payload.authorPosts];
+                state.tags = [...action.payload.topKeywords];
+            }
+        });
+    },
+});
+
+export const {} = blogSlice.actions;
+export default blogSlice.reducer;
